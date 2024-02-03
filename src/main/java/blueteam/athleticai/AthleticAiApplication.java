deb665cd836c421e235239e78698907cf9fd6cd2
Reece_Lu <53824755+Reece-Lu@users.noreply.github.com>
2024-02-03 00:57:54 -0800

    Initial commit

---
+++ src/main/java/blueteam/athleticai/AthleticAiApplication.java
@@ -0,0 +1,13 @@
+package blueteam.athleticai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AthleticAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AthleticAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/blueteam/athleticai/AthleticAiApplicationTests.java
@@ -0,0 +1,13 @@
+package blueteam.athleticai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AthleticAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
